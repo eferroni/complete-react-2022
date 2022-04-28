6b3e7edabe4204b27ca387da5698a19607cedbb8
Eduardo Ferroni <eduardoferroni@eduardoferroni1.mac.hpicorp.net>
2022-04-28 14:32:24 -0300

    apply redux-thunk middleware

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,26 @@
 import { CATEGORIES_ACTION_TYPE } from "./category.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPE.SET_CATEGORIES, categoriesArray);
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(
+    CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+
+export const fetchCategoriesFail = (error) =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAIL, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  try {
+    dispatch(fetchCategoriesStart());
+
+    const categoriesArray = await getCategoriesAndDocuments("categories");
+    dispatch(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    dispatch(fetchCategoriesFail(error));
+  }
+};
